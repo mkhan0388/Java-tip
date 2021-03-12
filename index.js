2b060dc52b7bcc9d1a290ea01837d2223b655196
mkhan0388 <79087325+mkhan0388@users.noreply.github.com>
2021-03-12 15:51:33 +0000

    added in the javascript
    set the variables, used documnet.write to add straight to page, then used concatenation to add the variables.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+let bill = 55.60
+let tip = (bill * 15) / 100
+let total = (bill + tip).toFixed(2)
 
+document.write(`<p> Your food bill is ${bill} with a tip of ${tip} giving a grand total of ${total} </p>`)
